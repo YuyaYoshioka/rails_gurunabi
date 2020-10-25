840039a9dda342d93526b22ee77e47f1a5e7bb4b
yuya <yuya3330930@yahoo.co.jp>
2020-10-25 15:30:24 +0900

    delete test dir

--- test/controllers/home_pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomePagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get home_pages_home_url
-    assert_response :success
-  end
-
-end
